aef0031896c6c570cf102207691c824affb9c306
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-06-26 12:16:17 +0900

    1. 데이터, 뷰 바인딩 true 2. 카카오 로그인 gradle 설정 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -62,4 +66,10 @@ dependencies {
 
     // logging-interceptor는 반환된 모든 응답에 대해 로그 문자열을 생성합니다.
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+
+    // kakao 로그인
+    implementation ("com.kakao.sdk:v2-user:2.11.1")
 }
