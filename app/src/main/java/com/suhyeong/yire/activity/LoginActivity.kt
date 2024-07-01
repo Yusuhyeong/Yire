0f1e74402801c81d812af0836676d8e919633bf3
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-07-01 15:00:09 +0900

    LoginActivity ViewModel에서 Activity 참조 수정

--- app/src/main/java/com/suhyeong/yire/activity/LoginActivity.kt
+++ app/src/main/java/com/suhyeong/yire/activity/LoginActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import com.suhyeong.yire.activity.viewmodel.LoginViewModel
 import com.suhyeong.yire.databinding.ActivityLoginBinding
+import com.suhyeong.yire.firebase.Firestore
 import com.suhyeong.yire.listener.LoginCheckListener
 import com.suhyeong.yire.test.MainTestActivity
 import com.suhyeong.yire.utils.Constants
 
-class LoginActivity : AppCompatActivity(), LoginCheckListener {
+class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val firestore = Firestore()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,6 @@ class LoginActivity : AppCompatActivity(), LoginCheckListener {
         setContentView(view)
 
         val viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
-        viewModel.setActivity(this, this)
         binding.view = viewModel
         binding.lifecycleOwner = this
 
@@ -34,9 +41,20 @@ class LoginActivity : AppCompatActivity(), LoginCheckListener {
             startActivity(intent)
             finish()
         }
+
+        KakaoSdk.init(this.applicationContext, "0690af0015f912d642074fbbf0afcf7f")
+
+        viewModel.try_kakao_login.observe(this) {
+            if (viewModel.try_kakao_login.value == true) {
+                loginCheck()
+                binding.clLoading.isVisible = true
+            } else {
+                viewModel.setDimVisibility(false)
+            }
+        }
     }
 
-    override fun onLoginSuccess(uid: String, nickName: String) {
+    fun onLoginSuccess(uid: String, nickName: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("uid", uid)
         intent.putExtra("nickName", nickName)
@@ -44,10 +62,95 @@ class LoginActivity : AppCompatActivity(), LoginCheckListener {
         finish()
     }
 
-    override fun onLoginFailure(error: String) {
-        if (Constants.UNREGISTERED_USER.equals(error))
+    fun onLoginFailure(error: String) {
+        binding.view?.setDimVisibility(false)
+        if (Constants.UNREGISTERED_USER.equals(error)) {
             Log.d("LoginActivity", "등록되지 않은 유저")
-        else if (Constants.KAKAO_LOGIN_ERROR.equals(error))
+        } else if (Constants.KAKAO_LOGIN_ERROR.equals(error)) {
             Log.d("LoginActivity", "카카오 로그인 실패")
+        }
+    }
+
+    fun loginCheck() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this.applicationContext)) {
+            UserApiClient.instance.loginWithKakaoTalk(this.applicationContext) { token, error ->
+                if (error != null) {
+                    Log.d("loginCheck", "카카오톡으로 로그인 실패! " + error.message)
+
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    UserApiClient.instance.loginWithKakaoAccount(
+                        this,
+                        callback = callback
+                    )
+
+                } else if (token != null) {
+                    Log.d("loginCheck", "카카오톡으로 로그인 성공! " + token.accessToken)
+                    fetchUserInfo()
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(
+                this.applicationContext,
+                callback = callback
+            )
+        }
+    }
+    private fun fetchUserInfo() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("fetchUserInfo", "사용자 정보 요청 실패: ${error.message}")
+            } else if (user != null) {
+                Log.d("fetchUserInfo", "사용자 정보 요청 성공: ${user.kakaoAccount?.profile?.nickname}")
+                Log.d("fetchUserInfo", "사용자 ID: ${user.id}")
+
+                val userId = user.id.toString()
+                Log.d("TEST_LOG", "uid : ${userId}")
+                firestore.checkUidAndNickname(userId) { uidExists, nicknameExists, nickname ->
+                    if (uidExists) {
+                        Log.d("TEST_LOG", "uid 있음")
+                        if (nicknameExists) {
+                            Log.d("TEST_LOG", "nickname 있음 ${nickname}")
+                            onLoginSuccess(userId, nickname.toString())
+                        } else {
+                            Log.d("TEST_LOG", "nickname 없음")
+                            val intent = Intent(this, NickNameActivity::class.java)
+                            intent.putExtra("uid", userId)
+                            startActivity(intent)
+                            finish()
+                        }
+                    } else {
+                        Log.d("TEST_LOG", "uid 없음")
+                        val intent = Intent(this, NickNameActivity::class.java)
+                        intent.putExtra("uid", userId)
+                        startActivity(intent)
+                        firestore.setUid(userId) { success ->
+                            if (success) {
+                                val intent = Intent(this, NickNameActivity::class.java)
+                                intent.putExtra("uid", userId)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Log.e("Login", "UID 저장 실패")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e("callback", "카카오 계정으로 로그인 실패! " + error.message)
+            Log.d("loginCheck", "로그인 실패")
+            onLoginFailure(Constants.KAKAO_LOGIN_ERROR)
+        } else if (token != null) {
+            Log.d("callback", "카카오 계정으로 로그인 성공! " + token.accessToken)
+            Log.d("loginCheck", "로그인 성공")
+            fetchUserInfo()
+        }
     }
 }
--- app/src/main/java/com/suhyeong/yire/activity/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/suhyeong/yire/activity/viewmodel/LoginViewModel.kt
@@ -17,114 +17,15 @@ import com.suhyeong.yire.listener.LoginCheckListener
 import com.suhyeong.yire.utils.Constants
 
 class LoginViewModel: ViewModel() {
-    private lateinit var _activity: AppCompatActivity
-    private lateinit var _listener: LoginCheckListener
-    private val _loading = MutableLiveData<Boolean>()
-    val loading: LiveData<Boolean> = _loading
-    private val firestore = Firestore()
+    private val _try_kakao_login = MutableLiveData<Boolean>()
+    val try_kakao_login: LiveData<Boolean> = _try_kakao_login
 
-    fun setActivity(activity: AppCompatActivity, listener: LoginCheckListener) {
-        _loading.value = false
-        _activity = activity
-        _listener = listener
+
+    fun loginKakao() {
+        _try_kakao_login.value = true
     }
 
-    fun statusUpdate() {
-        _loading.value = true
-        if (loginCheck())
-            Log.d("loginCheck", "로그인 성공")
-        else
-            Log.d("loginCheck", "로그인 실패")
-    }
-
-    fun loginCheck() : Boolean {
-        KakaoSdk.init(_activity.applicationContext, "0690af0015f912d642074fbbf0afcf7f")
-
-        var checkValue:Boolean = false
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(_activity.applicationContext)) {
-            UserApiClient.instance.loginWithKakaoTalk(_activity.applicationContext) { token, error ->
-                if (error != null) {
-                    Log.d("loginCheck", "카카오톡으로 로그인 실패! " + error.message)
-
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-
-                    UserApiClient.instance.loginWithKakaoAccount(
-                        _activity,
-                        callback = callback
-                    )
-
-                } else if (token != null) {
-                    Log.d("loginCheck", "카카오톡으로 로그인 성공! " + token.accessToken)
-                    checkValue = true
-                    _loading.value = false
-                    fetchUserInfo()
-                }
-            }
-        } else {
-            UserApiClient.instance.loginWithKakaoAccount(
-                _activity.applicationContext,
-                callback = callback
-            )
-        }
-
-        return checkValue
-    }
-
-    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (error != null) {
-            Log.e("callback", "카카오 계정으로 로그인 실패! " + error.message)
-            Log.d("loginCheck", "로그인 실패")
-            _listener.onLoginFailure(Constants.KAKAO_LOGIN_ERROR)
-            _loading.value = false
-        } else if (token != null) {
-            Log.d("callback", "카카오 계정으로 로그인 성공! " + token.accessToken)
-            Log.d("loginCheck", "로그인 성공")
-            _loading.value = false
-            fetchUserInfo()
-        }
-    }
-
-    private fun fetchUserInfo() {
-        UserApiClient.instance.me { user, error ->
-            if (error != null) {
-                Log.e("fetchUserInfo", "사용자 정보 요청 실패: ${error.message}")
-            } else if (user != null) {
-                Log.d("fetchUserInfo", "사용자 정보 요청 성공: ${user.kakaoAccount?.profile?.nickname}")
-                Log.d("fetchUserInfo", "사용자 ID: ${user.id}")
-
-                val userId = user.id.toString()
-                Log.d("TEST_LOG", "uid : ${userId}")
-                firestore.checkUidAndNickname(userId) { uidExists, nicknameExists, nickname ->
-                    if (uidExists) {
-                        Log.d("TEST_LOG", "uid 있음")
-                        if (nicknameExists) {
-                            Log.d("TEST_LOG", "nickname 있음 ${nickname}")
-                            _listener.onLoginSuccess(userId, nickname.toString())
-                        } else {
-                            Log.d("TEST_LOG", "nickname 없음")
-                            val intent = Intent(_activity, NickNameActivity::class.java)
-                            intent.putExtra("uid", userId)
-                            _activity.startActivity(intent)
-                        }
-                    } else {
-                        Log.d("TEST_LOG", "uid 없음")
-                        val intent = Intent(_activity, NickNameActivity::class.java)
-                        intent.putExtra("uid", userId)
-                        _activity.startActivity(intent)
-                        firestore.setUid(userId) { success ->
-                            if (success) {
-                                val intent = Intent(_activity, NickNameActivity::class.java)
-                                intent.putExtra("uid", userId)
-                                _activity.startActivity(intent)
-                            } else {
-                                Log.e("Login", "UID 저장 실패")
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    fun setDimVisibility(isVisible: Boolean) {
+        _try_kakao_login.value = isVisible
     }
 }
