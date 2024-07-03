d8631b56df1f6a7ac9438312d5aad83c02dabb04
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-07-03 15:10:05 +0900

    노래 검색 화면 임시 구현, api호출 시 fragment에서 값 전달 추가 (RecyclerView에 뿌리기 추후 구현)

--- app/src/main/java/com/suhyeong/yire/fragment/SearchFragment.kt
+++ app/src/main/java/com/suhyeong/yire/fragment/SearchFragment.kt
@@ -1,60 +1,59 @@
 package com.suhyeong.yire.fragment
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.suhyeong.yire.R
+import com.suhyeong.yire.activity.viewmodel.LoginViewModel
+import com.suhyeong.yire.databinding.ActivityLoginBinding
+import com.suhyeong.yire.databinding.FragmentCommonPopupBinding
+import com.suhyeong.yire.databinding.FragmentSearchBinding
+import com.suhyeong.yire.firebase.Firestore
+import com.suhyeong.yire.fragment.viewmodel.FragmentViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentSearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val viewModel = ViewModelProvider(this)[FragmentViewModel::class.java]
+        binding.searchViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.result.observe(viewLifecycleOwner, Observer { searchResults ->
+            searchResults?.let {
+                for (i: Int in 0 until searchResults.size) {
+                    Log.d("결과", "==============================================")
+                    Log.d("결과", "가수 : " + searchResults.get(i).artistName.toString())
+                    Log.d("결과", "해당 앨범 : " + searchResults.get(i).trackName.toString())
+                    Log.d("결과", "앨범 커버 : " + searchResults.get(i).artworkUrl100.toString())
+                    Log.d("결과", "출시일 : " + searchResults.get(i).releaseDate.toString())
+                    Log.d("결과", "장르 : " + searchResults.get(i).primaryGenreName.toString())
+                    Log.d("결과", "==============================================")
                 }
             }
+        })
+
+        return binding.root
     }
 }
